1f5e3e4d431a64cefa6e4e5f6cb8c5c544685196
Evgeny Svirsky <evgeny@cere.io>
2022-04-27 16:28:15 +0200

    * Minor fixes.

--- src/App.js
+++ src/App.js
@@ -1,64 +1,63 @@
 import './App.css';
-import {cereWebSDK} from "@cere/sdk-js/dist/web";
-import {useEffect, useState} from "react";
+import {cereWebSDK} from '@cere/sdk-js/dist/web';
+import {useEffect, useState} from 'react';
 
 function App() {
+  const [sdk, setSdk] = useState();
 
-    const [sdk, setSdk] = useState('No result');
+  useEffect(() => {
+    const LIVE_ONE_APP_ID = '2095';
 
-    useEffect(() => {
-        let sdk = cereWebSDK('2095', null, {
-            authMethod: {
-                type: 'TRUSTED_3RD_PARTY',
-                externalUserId: '12345', // replace with user id in Live One system
-                token: '1234567890' // <= hardcoded for integration phase only.
-                // After migration should be replaced with token provided by LiveOne
-            },
-            deployment: 'dev'
-        });
-
-        /**
-         * Specify the action after engagement data received.
-         * Please note: this action happens asynchronously.
-         */
-        sdk.onEngagement(onEngagementAction, {});
-
-        setSdk(sdk);
-    }, []);
+    let sdk = cereWebSDK(LIVE_ONE_APP_ID, null, {
+      authMethod: {
+        type: 'TRUSTED_3RD_PARTY',
+        externalUserId: '12345', // replace with user id in Live One system
+        token: '1234567890', // This token should be provided from authorized user by Live One
+      },
+      deployment: 'dev',
+    });
 
     /**
-     * Action to be triggered after engagement received.
+     * Specify the action after engagement data received.
+     * Please note: this action happens asynchronously.
      */
-    function onEngagementAction(template) {
-        var doc = document.getElementById('contentIFrame').contentWindow.document;
-        doc.open();
-        doc.write(template);
-        doc.close();
-    }
+    sdk.onEngagement(onEngagementAction);
 
-    async function sendSdkEvent() {
-        /**
-         * Send event to Cere system for to get user's NFT-.
-         */
-        let timestamp = Number(new Date())
+    setSdk(sdk);
+  }, []);
 
-        let signature = await sdk.signMessage(`${timestamp}`);
+  /**
+   * Action to be triggered after engagement received.
+   */
+  function onEngagementAction(template) {
+    const doc = document.getElementById('contentIFrame').contentWindow.document;
+    doc.open();
+    doc.write(template);
+    doc.close();
+  }
 
-        sdk.sendEvent('LIVE_ONE_LIST_OF_NFTS', {
-            timestamp,
-            signature
-        });
-    }
+  const sendSdkEvent = async () => {
+    /**
+     * Send event to Cere system for to get user's NFT-.
+     */
+    let timestamp = Number(new Date());
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <button onClick={sendSdkEvent}>Send Sdk Event</button>
-                <iframe title={"Action Window"} id="contentIFrame" width={400} height={700} frameBorder={3} color={'white'}>
-                </iframe>
-            </header>
-        </div>
-    );
+    let signature = await sdk.signMessage(`${timestamp}`);
+
+    sdk.sendEvent('LIVE_ONE_LIST_OF_NFTS', {
+      timestamp,
+      signature,
+    });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={sendSdkEvent}>Send Sdk Event</button>
+        <iframe title={'Action Window'} id="contentIFrame" width={400} height={700} frameBorder={3} color={'white'} />
+      </header>
+    </div>
+  );
 }
 
 export default App;
